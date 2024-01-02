f42cc68b5b0a3e9d2a956e09c117fc7f889c92ef
Georgios Rounis <georg.rounis@gmail.com>
2024-01-03 00:17:06 +0100

    implement ip-api endpoint

--- controllers/ipAddress.mjs
+++ controllers/ipAddress.mjs
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 
+// retrieve IP address from api.ipify api - Not used since ip-api is used instead
 const findIPAddress = (req,res) => {
   try {
     fetch('https://api.ipify.org?format=json')
@@ -14,4 +15,17 @@ const findIPAddress = (req,res) => {
   }
 }
 
-export { findIPAddress };
+//Retrieve IP address, country and city from ip-api
+const handleIPAddressCountryAndCity = (req,res) => {
+  try {
+    fetch('http://ip-api.com/json/?fields=61439')
+      .then((response) => response.json())
+      .then((data) => {console.log(data);
+        res.json(data);
+      })
+  } catch(error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+export { findIPAddress, handleIPAddressCountryAndCity };
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors'
-import {findIPAddress} from './controllers/ipAddress.mjs'
+import {findIPAddress, handleIPAddressCountryAndCity} from './controllers/ipAddress.mjs'
 
 const port = 3000;
 const app = express();
 app.use(cors());
 
 app.get('/getip', (req, res) => {findIPAddress(req,res);});
+app.get('/ipapi',(req,res) => {handleIPAddressCountryAndCity(req,res)});
 
 app.listen(port, () => {
     console.log(`app is running on port ${port}`);
