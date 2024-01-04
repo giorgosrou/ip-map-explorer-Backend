c1032eb42e49ed8fe31ea1c96b5ddeb0157a1665
Georgios Rounis <georg.rounis@gmail.com>
2024-01-04 19:28:35 +0100

    trial and error

--- controllers/ipAddress.mjs
+++ controllers/ipAddress.mjs
@@ -1,6 +1,9 @@
 import fetch from 'node-fetch';
 
 const handleIPAddressCountryAndCity = (req, res, db) => {
+  //const userIP = req.ip;
+  //console.log(userIP);
+  //console.log(`http://ip-api.com/json/${userIP}`)
   fetch('http://ip-api.com/json/?fields=61439')
     .then((response) => response.json())
     .then((data) => {
